4bc7289b09d1bd8389856b8c7b04f5fd680e8781
prudhvichigurupati <prudhvichigurupati88@gmail.com>
2022-10-26 19:04:44 -0500

    added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata');
+var computationRouter = require('./routes/computation');
 
 var app = express();
 
